cbe15d8bcbbec1ed3c3b20491a2c627bdf532318
Alcielma <alcielmaluzinete@gmail.com>
2026-01-22 15:13:38 -0300

    foi adicionado o fundo e atualizado os campos para o jogo

--- UI/mainMenu.go
+++ UI/mainMenu.go
@@ -11,7 +11,7 @@ type App struct {
 
 func NewApp() *App {
 	// troque rows/cols dinamicamente conforme quiser
-	return &App{current: NewBoardUI(10, 10)}
+	return &App{current: NewDualBoardUI(10, 10)}
 }
 
 func (a *App) Update() error              { return a.current.Update() }
@@ -19,7 +19,7 @@ func (a *App) Draw(screen *ebiten.Image)  { a.current.Draw(screen) }
 func (a *App) Layout(w, h int) (int, int) { return a.current.Layout(w, h) }
 
 func Run() error {
-    ebiten.SetWindowSize(800, 600)
+    ebiten.SetWindowSize(screenWidth, screenHeight)
     ebiten.SetWindowTitle("Battleship - Preview")
     return ebiten.RunGame(NewApp())
 }
