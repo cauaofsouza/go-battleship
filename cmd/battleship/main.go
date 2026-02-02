23221a27e3f882a0e246acf3b30978254683c499
Alcielma <alcielmaluzinete@gmail.com>
2026-02-02 08:52:46 -0300

    feat: criação do board

--- cmd/battleship/main.go
+++ cmd/battleship/main.go
@@ -1,65 +1,17 @@
 package main
 
 import (
-	//"gobattleship/UI"
-	"gobattleship/game"
-	"gobattleship/internal/service"
-	"fmt"
 	"log"
+
+	"github.com/allanjose001/go-battleship/game"
+	"github.com/hajimehoshi/ebiten/v2"
 )
 
 func main() {
-	//fmt.Println("Backend is running...");
-	
-	//UI.Run();
-
-	board1 := new(game.Board);
-
-	//game.PrintBoard(board1);
-
-	barco1 := new(game.Ship);
-	barco1.Size = 3;
-	barco1.Horizontal = true;
-
-	barco2 := new(game.Ship);
-	barco2.Size = 3;
-
-	fmt.Println("");
-
-	game.PlaceShip(board1, barco1, 1, 1);
-
-	//game.PrintBoard(board1);
-
-	fmt.Println("");
-
-	//fmt.Println("hit count barco1:", barco1.HitCount);
-
-	game.AttackPosition(board1, 1, 1);
-
-	//fmt.Println("hit count barco1:", barco1.HitCount);
-
-	//========== teste de profile ===========
-
-	profile1 := new(service.Profile);
-	profile1.Username = "Player1";
-	profile1.TotalScore = 200
-	profile1.HighestScore = 50
-	profile1.GamesPlayed = 5
-	profile1.MedalsEarned = 2
-
-	service.SaveProfile(*profile1);
-	err := service.SaveProfile(*profile1)
-	if err != nil {
+	ebiten.SetWindowSize(900, 650)
+	ebiten.SetWindowTitle("Fúria dos Mares")
+	g := game.NewGame()
+	if err := ebiten.RunGame(g); err != nil {
 		log.Fatal(err)
 	}
-
-	profile2, err := service.FindProfile("Player2");
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Printf("perfil encontrado: %+v\n", profile2);
-
-	//service.RemoveProfile("Player1");
-
 }
--- game/game.go
+++ game/game.go
@@ -1,27 +1,38 @@
 package game
 
+//estado global do jogo
+
 import (
 	"image/color"
 
+	"github.com/allanjose001/go-battleship/game/scenes"
+	"github.com/allanjose001/go-battleship/game/state"
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
 type Game struct {
+	scene *scenes.BattleScene
 }
 
 func NewGame() *Game {
-	return &Game{}
+	st := state.NewGameState()
+	return &Game{scene: scenes.NewBattleScene(st)}
 }
 
 func (g *Game) Update() error {
-	// lógica de jogo (vazia por enquanto)
+	if g.scene != nil {
+		return g.scene.Update()
+	}
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	screen.Fill(color.RGBA{R: 30, G: 30, B: 30, A: 255})
+	screen.Fill(color.RGBA{R: 122, G: 133, B: 180, A: 255})
+	if g.scene != nil {
+		g.scene.Draw(screen)
+	}
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
-	return 800, 600
+	return 900, 650
 }
